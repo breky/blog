88bed657fb0d3670acf93f572ee1ab763dbf3309
Breky <sk_sword@sina.com>
2023-03-11 09:09:32 +0800

    bundle lock

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
-self.importScripts('/assets/js/data/swcache.js');
+self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230310.105459';
+const cacheName = 'chirpy-20230311.085702';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
